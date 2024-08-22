6bd7c833459abb189da19c63ea0b9844b3ee7615
Aman Niyad <101011357+AmanNiyad@users.noreply.github.com>
2024-08-22 12:26:23 +0530

    Add files via upload

--- img_seg.m
+++ img_seg.m
@@ -2,16 +2,26 @@ clc
 close all
 clear all
 
-pkg load image
+% pkg load image
 
-img = rgb2gray(imread("00187.jpg"));
+% img = rgb2gray(imread("00187.jpg"));
+img = im2gray(imread("00187.jpg"));
+
 % imshow(img)
 
 % In matlab, use imbinarize for segmentation.
-BW = im2bw(img, graythresh(img));
+BW = imbinarize(img);
+% BW = im2bw(img, graythresh(img));
+BW = ~BW;
 
 % In matlab, use montage for multiple image display.
-figure();
-subplot(121);imshow(img);
-subplot(122);imshow(BW);
+% figure();
+% subplot(121);imshow(img);
+% subplot(122);imshow(BW);
 
+montage({img, BW})
+nnz(BW)
+
+maskedImg = img;
+maskedImg(~BW) = 0;
+imshow(maskedImg)
